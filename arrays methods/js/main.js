21eaca58215dd87b0053b0f3c5a06777ccf7073b
macbook <macbook@MacBook-Pro.local>
2023-03-21 21:10:45 +0500

    arrays push

---
+++ arrays methods/js/main.js
@@ -0,0 +1,23 @@
+//? Array methods
+// const users = ["Ibrohim", "Ravshan", "Ozodbek", "Ismoil", "Abror", "Aziz"];
+//! push => array oxiridan ma'lumot qo'shadi
+// users.push("Abdulla");
+// console.log(users.push("Sardorbek")); // array length => 7
+// console.log(users);
+//! pop => array oxiridan bitta ma'lumot olib tashlaydi
+// console.log(users.pop()); // Aziz
+// console.log(users);
+
+//! unshift => array boshidan ma'lumot qo'shadi
+// console.log(users);
+// console.log(users.unshift("Hello")); // array length => 7
+// console.log(users);
+
+//! shift => array boshidan bitta ma'lumot olib tashlaydi
+// console.log(users);
+// console.log(users.shift()); // Ibrohim
+// console.log(users);
+
+// const users = ["Ibrohim", "Ravshan", "Ozodbek", "Ismoil", "Abror", "Aziz"];
+// console.log(users.indexOf("Ravshan"));
+// console.log(users.includes("Abror"))
