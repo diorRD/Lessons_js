b304f9360d4edcd876e6a78006c1fed44a08f49f
macbook <macbook@MacBook-Pro.local>
2023-03-23 12:47:21 +0500

    string methods

---
+++ String methods/js/main.js
@@ -0,0 +1,21 @@
+//? String methods
+// const username = "Userbek";
+//* String.length
+// console.log(username.length);
+// console.log(username["length"]);
+//* String.trim()
+// const username2 = "Userbek";
+// console.log(username2);
+// console.log(username2.trim());
+//* String.toUpperCase();
+// console.log(username.toUpperCase());
+//* String.toLowerCase();
+// console.log(username.toLowerCase());
+//* String.substring(x,y);
+// console.log(username.substring(0, 4));
+//* String.split();
+// const str = "Ibrohim 20 WIUT Najot-ta'lim";
+// console.log(str);
+// console.log(str.split(" "));
+// //* charAt(n)
+// console.log(username.charAt(6));
